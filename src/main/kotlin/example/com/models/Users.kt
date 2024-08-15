a9af90741f6ad32b5a5976c5b2011cf0f812b054
mic050r <s2216@e-mirim.hs.kr>
2024-08-15 22:07:26 +0900

    ✨FEAT(#3) :: User 테이블 정의

---
+++ src/main/kotlin/example/com/models/Users.kt
@@ -0,0 +1,16 @@
+package example.com.models
+
+import org.jetbrains.exposed.sql.Table
+import kotlinx.serialization.Serializable
+
+// Exposed ORM 테이블 정의
+object Users : Table() {
+    val id = integer("id").autoIncrement()
+    val name = varchar("name", 50)
+    val age = integer("age")
+    override val primaryKey = PrimaryKey(id)
+}
+
+// 데이터 클래스 정의 (직렬화를 위해 Serializable 사용)
+@Serializable
+data class User(val id: Int? = null, val name: String, val age: Int)
